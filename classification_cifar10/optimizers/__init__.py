7950e71b1f0620060cc4c9cea30a693ba6c323cb
Sharan Ramjee <sharanramjee@gmail.com>
2021-05-14 16:38:46 -0700

    Added classification CIFAR-10 experiments

---
+++ classification_cifar10/optimizers/__init__.py
@@ -0,0 +1,7 @@
+from .AdaBelief import AdaBelief
+from .Fromage import Fromage
+from .Yogi import Yogi
+from .MSVAG import MSVAG
+from .RAdam import RAdam
+from .AdamW import AdamW
+from .AdaLA import AdaLA
